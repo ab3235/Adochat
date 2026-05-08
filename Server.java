3c95828be7d9919e39ee2f63b2cb22ef4d95d084
ab3235 <ab3235@njit.edu>
2026-05-08 19:02:27 -0400

    added warnings and a kick system

--- Server.java
+++ Server.java
@@ -7,7 +7,7 @@ public class Server {
     private static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName("127.0.0.1"));
+        ServerSocket serverSocket = new ServerSocket(PORT, 1, InetAddress.getByName("127.0.0.1"));
         System.out.println("Server started on port " + PORT);
 
         Socket clientSocket = serverSocket.accept();
@@ -16,10 +16,21 @@ public class Server {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+        // track the warns
+        Warnings warnings = new Warnings();
+
+        //tracks users
+        String username = in.readLine();
+        System.out.println("User joined: " + username);
+
         String message;
         while ((message = in.readLine()) != null) {
+            // filter warn
             if (Filter.isFlagged(message)) {
-                out.println("[WARNING] Message blocked: Filtered message");
+                if (warnings.addWarning(out)) {
+                    System.out.println("Client has been kicked for too many warns");
+                    break;
+                }
             } else {
                 System.out.println("Client: " + message);
                 out.println("Server received: " + message);
---
+++ Warnings.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+public class Warnings {
+
+    private int warnings = 0;
+    private final int maxWarnings = 3;
+
+    // Warning Sys, loops until true until client is kicked
+    public boolean addWarning(PrintWriter out) {
+        warnings++;
+        int remaining = maxWarnings - warnings; // Remaining warning to max warnings // how many warns the user has
+        // kick if warnings get to a maxWarning Amount
+        if (warnings >= maxWarnings) {
+            out.println("WARNING! Repeat Offender detected. Teleporting to the Shadow Realms... [KICKED]");
+            return true;
+
+        } //  Else if warned shows amounts of remaining warns left
+        else {
+            out.println("[WARNING " + warnings + "/" + maxWarnings + "] Prohibited message blocked. " + remaining + " warning(s) remaining.");
+            return false;
+        }
+    }
+}
